3b96b45196ac02635645f874bf7e84321a657804
Tsunamins <reillyamr@gmail.com>
2020-02-09 14:30:08 -0500

    add test to front end in App.js

--- expense-frontend/src/App.js
+++ expense-frontend/src/App.js
@@ -3,6 +3,19 @@ import React from 'react';
 //import './App.css';
 
 class App extends React.Component {
+
+  //an initial test, temporary:
+  componentDidMount(){
+    fetch('http://localhost:3000/api/v1/accounts', {
+   
+      
+
+    })
+    .then(resp => resp.json())
+    .then(data => console.log(data[0]))
+  
+  }
+
   render(){
     return (
       <div className="App">
