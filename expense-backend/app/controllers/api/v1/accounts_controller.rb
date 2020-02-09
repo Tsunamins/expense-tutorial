153efd22b40d4ff2109b019a85febdb1078584da
Tsunamins <reillyamr@gmail.com>
2020-02-09 13:49:52 -0500

    set up controllers

--- expense-backend/app/controllers/api/v1/accounts_controller.rb
+++ expense-backend/app/controllers/api/v1/accounts_controller.rb
@@ -1,2 +1,39 @@
 class Api::V1::AccountsController < ApplicationController
+
+      def index
+        @accounts = Account.all
+        render json: @accounts
+      end
+  
+      def create
+        @account = Account.new(account_params) # new spec allows account params from private below
+        if @account.save #if saved successfully, here session will have additionaly statements
+          render json: @account
+        else
+          render json: {error: 'Error creating account'}
+        end
+      end
+  
+      def show
+        @account = Account.find(params[:id])
+        render json: @account
+      end
+  
+      def destroy
+        @account = Account.find(params[:id])
+        @account.destroy
+      end
+  
+      def update
+        @account = Account.find(params[:id])
+        @account.update(name: params["account"]["name"])
+        @account.save
+        render json: @account
+      end
+  
+      private
+  
+      def account_params
+        params.require(:account).permit(:name, :balance)
+      end
 end
--- expense-backend/app/controllers/api/v1/transactions_controller.rb
+++ expense-backend/app/controllers/api/v1/transactions_controller.rb
@@ -1,2 +1,53 @@
 class Api::V1::TransactionsController < ApplicationController
+    #this method setup below to allow use of @account and its model methods to be accessesed in
+    #conjunction with transaction, esp helpful within create method below
+    before_action :set_account
+
+    def index
+    #previous was:
+    #@transactions = @account.transactions
+      @transactions = Transaction.all
+      render json: @transactions
+    end
+  
+    def show
+       
+      @transaction = Transaction.find(params[:id])
+      render json: @transaction
+    end
+  
+    def create
+      @transaction = @account.transactions.new(transaction_params)
+  
+      @transaction.date = DateTime.now
+      #@account is an account instance so its method written in Account model can be called upon it here, passing the transaction instance into it
+      if @account.update_balance(@transaction) != 'Balance too low.'
+        @transaction.save
+        render json: @account
+      else
+        render json: {error: 'Balance too low'}
+      end
+    end
+  
+    def destroy
+      @transaction = Transaction.find(params["id"])
+      @account = Account.find(@transaction.account_id)
+      if @account.update_balance_on_delete(@transaction)
+        @transaction.destroy
+        render json: @account
+      else
+        render json: {error: 'Balance too low'}
+      end
+    end
+  
+    private
+  
+    def set_account
+      @account = Account.find(params[:account_id])
+    end
+  
+  
+    def transaction_params
+      params.require(:transaction).permit(:amount, :account_id, :kind, :date, :description)
+    end
 end
