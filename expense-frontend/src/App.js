f04e7d9a3a40bb5396fa0b6469a855f4244d8175
Tsunamins <reillyamr@gmail.com>
2020-02-09 15:10:25 -0500

    comment out example

--- expense-frontend/src/App.js
+++ expense-frontend/src/App.js
@@ -5,13 +5,13 @@ import React from 'react';
 class App extends React.Component {
 
   //an initial test, temporary, start with just data then check other aspects in this case data.transactions:
-  componentDidMount(){
-    fetch('http://localhost:3000/api/v1/accounts/1', {
-    })
-    .then(resp => resp.json())
-    .then(data => console.log(data.transactions))
+  // componentDidMount(){
+  //   fetch('http://localhost:3000/api/v1/accounts/1', {
+  //   })
+  //   .then(resp => resp.json())
+  //   .then(data => console.log(data.transactions))
   
-  }
+  // }
 
   render(){
     return (
