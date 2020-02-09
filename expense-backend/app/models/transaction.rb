4bafc557722937300ba4f293446def29767b7a6d
Tsunamins <reillyamr@gmail.com>
2020-02-09 12:32:13 -0500

    add more validations

--- expense-backend/app/models/transaction.rb
+++ expense-backend/app/models/transaction.rb
@@ -2,4 +2,6 @@ class Transaction < ApplicationRecord
     belongs_to :account
 
     validates :amount, presence: true
+
+    validates_inclusion_of :kind, in => ['deposit', 'withdraw'] #what is referenced in kind must be deposit or withdraw
 end
