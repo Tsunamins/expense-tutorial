2e12fb1eae8cbe9eaf8ddcbad4a903fe7b5d2d38
Tsunamins <reillyamr@gmail.com>
2020-02-09 14:55:16 -0500

    format date in serializer

--- expense-backend/app/serializers/transaction_serializer.rb
+++ expense-backend/app/serializers/transaction_serializer.rb
@@ -1,3 +1,8 @@
 class TransactionSerializer < ActiveModel::Serializer
   attributes :id, :account_id, :amount, :kind, :date, :description
+
+  #added to format date appearance:
+  def date
+    self.object.date.strftime("%m/%d/%Y %I:%M%p")
+  end
 end
