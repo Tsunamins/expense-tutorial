0a82e6d7c4b613069f7a6ee3eff84dbe44eabe49
Tsunamins <reillyamr@gmail.com>
2020-02-09 14:55:06 -0500

    add to Account serializer transaction relationship

--- expense-backend/app/serializers/account_serializer.rb
+++ expense-backend/app/serializers/account_serializer.rb
@@ -1,3 +1,6 @@
 class AccountSerializer < ActiveModel::Serializer
-  attributes :id, :name, :balance
+  attributes :id, :name, :balance #these are here to allow access from front end, what attributes to access from account, if these were not here would see the created and updated at attributes
+
+  #added so that can access data.transactions in front end from fetch:
+  has_many :transactions
 end
