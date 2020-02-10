d27948cf626a023911877765990dea993b7f9519
Tsunamins <reillyamr@gmail.com>
2020-02-10 13:10:23 -0500

    annotated fetchAccounts

--- expense-frontend/src/actions/fetchAccounts.js
+++ expense-frontend/src/actions/fetchAccounts.js
@@ -1,10 +1,11 @@
 export function fetchAccounts() {
+    //return dispatch (built in) as an argument, a thunk function 
     return (dispatch) => {
       fetch('http://localhost:3000/api/v1/accounts')
       .then(resp => resp.json())
-      .then(accounts => dispatch({
-        type: 'FETCH_ACCOUNTS',
-        payload: accounts
+      .then(accounts => dispatch({ //brought in here to wait for connection from backend, rather than automatcially dispatching in connect
+        type: 'FETCH_ACCOUNTS',  //dispatch must have at least a type, 
+        payload: accounts       //and since getting and using data they next key needed is payload
       }))
     }
   }
