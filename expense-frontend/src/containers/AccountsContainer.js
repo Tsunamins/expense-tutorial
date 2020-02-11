a200f4d699d48f7d0933a001c6a21274ff5b1191
Tsunamins <reillyamr@gmail.com>
2020-02-11 10:35:22 -0500

    annotate accountsContainer

--- expense-frontend/src/containers/AccountsContainer.js
+++ expense-frontend/src/containers/AccountsContainer.js
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import {connect} from 'react-redux'
-import {Route, Switch} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom' //allows access to Route tag, bc all of this is wrapped in Router from index
 import {fetchAccounts} from '../actions/fetchAccounts'
 import Accounts from '../components/Accounts'
 import Account from '../components/Account'
@@ -22,7 +22,9 @@ class AccountsContainer extends React.Component {
             <NavBar/>
             <Switch>
               <Route path='/accounts/new' component={AccountInput}/>
+              {/* routerProps allows passing of these props in in additon to props set up this.props.accoutns, ~adds build in props */}
               <Route path='/accounts/:id' render={(routerProps) => <Account {...routerProps} accounts={this.props.accounts}/>}/>
+              {/* this.props.accounts allows listing of all accounts, below also originally used Route exact path */}
               <Route path='/accounts' render={(routerProps) => <Accounts {...routerProps} accounts={this.props.accounts}/>}/>
             </Switch>
 
